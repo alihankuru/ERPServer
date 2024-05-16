198a39c3135f1208d6be5851cdc1d1623300cae5
AK <ali.kuru6@hotmail.com>
2024-05-16 17:07:20 +0300

    -

---
+++ ERPServer.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ERPServer.Application.Features.Customers.CreateCustomer;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Customers.UpdateCustomer
+{
+    public sealed record class UpdateCustomerCommand(
+        Guid Id,
+        string Name,
+        string TaxDepartment,
+        string TaxNumber,
+        string City,
+        string Town,
+        string FullAddress) : IRequest<Result<string>>;
+}
---
+++ ERPServer.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ERPServer.Domain.Repositories;
+using FluentValidation;
+using GenericRepository;
+
+namespace ERPServer.Application.Features.Customers.UpdateCustomer
+{
+
+
+
+    public sealed class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+    {
+        public UpdateCustomerCommandValidator()
+        {
+            RuleFor(p => p.TaxNumber).MinimumLength(10).MaximumLength(11);
+            RuleFor(p => p.Name).MinimumLength(3);
+
+        }
+    }
+
+}
