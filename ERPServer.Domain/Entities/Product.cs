378de187f712d358f8b7099ca172683e77360812
AK <ali.kuru6@hotmail.com>
2024-06-04 22:46:49 +0300

    -

---
+++ ERPServer.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using ERPServer.Domain.Abstractions;
+using ERPServer.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Domain.Entities
+{
+    public class Product:Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public ProductTypeEnum Type { get; set; } = ProductTypeEnum.Product;
+
+
+    }
+}
